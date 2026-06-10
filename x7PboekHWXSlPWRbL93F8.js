3b37e236ab8de3489b88108b3b773fcb7e08db94
Uxidone <zihmanisoskars05@gmail.com>
2026-06-10 20:36:48 +0300

    Update x7PboekHWXSlPWRbL93F8.js

--- x7PboekHWXSlPWRbL93F8.js
+++ x7PboekHWXSlPWRbL93F8.js
@@ -1,7 +1,7 @@
 const appVersion = "V1.22.32"
 const appID = "Undefined"
 const lastEdited = "10th of June 2026"
-document.write(`App version ${appVersion} App ID ${appID} Last edited on ${lastEdited} ${lasteditedTime}`)
+document.write(`App version ${appVersion} App ID ${appID} Last edited on ${lastEdited}, ${lasteditedTime}`)
 const appDescription = "Krutly Anarchy Minecraft server"
 const appTags = "Krutly, Anarchy, Minecraft, Minecraft Server"
 const metaDescription = "Krutly Anarchy Minecraft server is a server where you can hack cheat and other."
