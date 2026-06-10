ce44cfdaf49957427e991bb9c0504919813c25e4
Uxidone <zihmanisoskars05@gmail.com>
2026-06-10 17:03:18 +0300

    Update x7PboekHWXSlPWRbL93F8.js

--- x7PboekHWXSlPWRbL93F8.js
+++ x7PboekHWXSlPWRbL93F8.js
@@ -1,4 +1,7 @@
 const appVersion = "V1.22.32"
 const appID = "Undefined"
 const lastEdited = "10th of June 2026"
-document.write(`${appVersion} ${appID} ${lastEdited}`)
+document.write(`App version ${appVersion} App ID ${appID} Last edited on ${lastEdited}`)
+const appDescription = "Krutly Anarchy Minecraft server"
+const appTags = "Krutly, Anarchy, Minecraft, Minecraft Server"
+
